4049b75e0c904dedb7f5d1fdc17b20b03b07bd26
Bar Bar Jinks <54543475+PalliateAi@users.noreply.github.com>
2021-06-02 13:39:32 +0100

    First Commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with Zero Fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without any fees",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Rocket",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transaction with Zero Fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without any fees",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-2.svg").default,
+  alt: "People",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with Zero Fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without any fees",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-3.svg").default,
+  alt: "Card",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
