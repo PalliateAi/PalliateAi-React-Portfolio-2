8d3ce08ea87edaba62b06668f2151154f730a00a
Bar Bar Jinks <54543475+PalliateAi@users.noreply.github.com>
2021-06-20 15:20:01 +0100

    Footer Links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -36,40 +36,160 @@ const Footer = () => {
               <FooterLinkItems>
                 <FooterLinkTitle>About Us</FooterLinkTitle>
 
-                <FooterLink to="/signin">About Us</FooterLink>
-                <FooterLink to="/signin">How it Works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms of Service</FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  About Us
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  How it Works
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Testimonials
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Careers
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Investors
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Terms of Service
+                </FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
                 <FooterLinkTitle>Contact Us</FooterLinkTitle>
 
-                <FooterLink to="/signin">Contact</FooterLink>
-                <FooterLink to="/signin">Support</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Sponsorships</FooterLink>
-                <FooterLink to="/signin">Destinations</FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Contact
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Support
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Testimonials
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Sponsorships
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Destinations
+                </FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
             <FooterLinksWrapper>
               <FooterLinkItems>
                 <FooterLinkTitle>Videos</FooterLinkTitle>
 
-                <FooterLink to="/signin">Submit Videos</FooterLink>
-                <FooterLink to="/signin">Ambassadors</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Influencers</FooterLink>
-                <FooterLink to="/signin">Agency</FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Submit Videos
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Ambassadors
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Testimonials
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Influencers
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Agency
+                </FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
                 <FooterLinkTitle>Social Media</FooterLinkTitle>
 
-                <FooterLink to="/signin">Instagram</FooterLink>
-                <FooterLink to="/signin">Youtube</FooterLink>
-                <FooterLink to="/signin">Facebook</FooterLink>
-                <FooterLink to="/signin">Twitter</FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Instagram
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Youtube
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Facebook
+                </FooterLink>
+                <FooterLink
+                  to={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="CV Website"
+                >
+                  Twitter
+                </FooterLink>
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
@@ -83,19 +203,39 @@ const Footer = () => {
                 All rights reserved.
               </WebsiteRights>
               <SocialIcons>
-                <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <SocialIconLink
+                  href={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="Facebook"
+                >
                   <FaFacebook />
                 </SocialIconLink>
-                <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <SocialIconLink
+                  href={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="Instagram"
+                >
                   <FaInstagram />
                 </SocialIconLink>
-                <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <SocialIconLink
+                  href={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="Youtube"
+                >
                   <FaYoutube />
                 </SocialIconLink>
-                <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <SocialIconLink
+                  href={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="Twitter"
+                >
                   <FaTwitter />
                 </SocialIconLink>
-                <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+                <SocialIconLink
+                  href={"//www.eddyku.com/"}
+                  target="_blank"
+                  aria-label="LinkedIn"
+                >
                   <FaLinkedin />
                 </SocialIconLink>
               </SocialIcons>
